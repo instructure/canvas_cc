cbba73159b2adc6bad05b8af4920466dced57d09
Brandon Broschinsky <brandonbr@instructure.com>
2014-11-24 08:53:20 -0700

    use standard methods for file upload
    Change-Id: I0893eddab26bce2260500ffc66776dcabf18b76c
    refs: PFS-510

--- lib/canvas_cc/canvas_cc/file_upload_question_writer.rb
+++ lib/canvas_cc/canvas_cc/file_upload_question_writer.rb
@@ -6,14 +6,13 @@ module CanvasCc::CanvasCC
     private
 
     def self.write_question_item_xml(node, question)
-      node.item(:title => question.title, :ident => question.identifier) do |item_node|
-        write_qti_metadata(item_node, question)
-        item_node.presentation do |presentation_node|
-          presentation_node.material do |material_node|
-            material_node.mattext(question.material, :texttype => 'text/html')
-          end
-        end
-      end
+      super(node, question)
+    end
+
+    def self.write_responses(presentation_node, question)
+    end
+
+    def self.write_response_conditions(processing_node, question)
     end
   end
 end
